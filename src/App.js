b35ae8f6845b682817701952828932fdd19a0104
wuyaheng <wuyaheng2016@gmail.com>
2020-09-24 22:58:39 -0400

    add params to api

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import "./App.css";
 
 class App extends Component {
   state = {
+    ofns_desc: [],
+    sel_ofns: "",
+    offet: 0,
+    limit: 100,
     results: [],
     filtered: [],
   };
@@ -15,30 +19,59 @@ class App extends Component {
     this.searchArrestType();
   }
 
-  searchArrestType = () => {
-    axios({
-      method: "GET",
-      url: "https://data.cityofnewyork.us/resource/8h9b-rp9u.json",
-      headers: {
-        "content-type": "application/json",
-      },
-    })
-      .then((res) => {
-        this.setState({
-          results: res.data,
-          filtered: res.data,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
+  searchArrestType = async () => {
+    try {
+      const res = await axios.get(
+        "https://data.cityofnewyork.us/resource/8h9b-rp9u.json?$group=ofns_desc&$select=ofns_desc"
+      );
+      this.setState({
+        ofns_desc: res.data.map((x) => x.ofns_desc),
       });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  advanceOffset = (val) => {
+    if (val) {
+      this.setState(
+        {
+          offet: this.state.offet + this.state.limit,
+        },
+        this.searchArrests
+      );
+    } else if (this.state.offet - this.state.limit >= 0) {
+      this.setState(
+        {
+          offet: this.state.offet - this.state.limit,
+        },
+        this.searchArrests
+      );
+    }
+  };
+
+  searchArrests = async () => {
+    const res = await axios.get(
+      `https://data.cityofnewyork.us/resource/8h9b-rp9u.json?$limit=${this.state.limit}&$offset=${this.state.offet}`,
+      {
+        params: {
+          $order: "arrest_date DESC",
+          ofns_desc: this.state.sel_ofns,
+        },
+      }
+    );
+    this.setState({
+      filtered: res.data,
+    });
   };
 
   handleInputChange = (event) => {
-    let value = event.target.value;
-    this.setState({
-      filtered: this.state.results.filter((x) => x.ofns_desc === value),
-    });
+    this.setState(
+      {
+        sel_ofns: event.target.value,
+      },
+      this.searchArrests
+    );
   };
 
   render() {
@@ -46,38 +79,60 @@ class App extends Component {
       <>
         <nav className="navbar navbar-light bg-dark">
           <span className="navbar-brand mb-0 h1 text-white pt-1">
-          NYC Historic Arrest Data Visualization
+            NYC Historic Arrest Data Visualization
           </span>
-        </nav> 
+        </nav>
         <div className="container">
           <div className="row mt-2">
             <div className="col-md-4">
               <SearchForm
-                results={this.state.results}
+                results={this.state.ofns_desc}
                 handleInputChange={this.handleInputChange}
               />
-              <div class="alert alert-danger" role="alert">
+              <div className="alert alert-danger" role="alert">
                 Number of Arrests: {this.state.filtered.length}
               </div>
-             
+
               <p>
-              NYC Historic Arrest Data Visualization is a dashboard that displays every arrest effected in NYC by the NYPD going back to 2006 through 2018. Users can select an arrest type from the drop-down list; the location of each arrest will then be displayed on the map with their arrest date. Meanwhile, the aggregate number of arrests in each borough will be displayed on one bar chart, with the number of arrestees in each age range displayed on another bar chart. At the bottom, a line graph is used to show the number of arrests over time. 
+                NYC Historic Arrest Data Visualization is a dashboard that
+                displays every arrest effected in NYC by the NYPD going back to
+                2006 through 2018. Users can select an arrest type from the
+                drop-down list; the location of each arrest will then be
+                displayed on the map with their arrest date. Meanwhile, the
+                aggregate number of arrests in each borough will be displayed on
+                one bar chart, with the number of arrestees in each age range
+                displayed on another bar chart. At the bottom, a line graph is
+                used to show the number of arrests over time.
               </p>
-         
-              <p>Data Source: <a href="https://data.cityofnewyork.us/Public-Safety/NYPD-Arrests-Data-Historic-/8h9b-rp9u" aria-label="NYCOpenData" title="NYCOpenData" target="_blank" rel="noopener noreferrer">NYC OpenData</a></p>
-             
+
+              <p>
+                Data Source:{" "}
+                <a
+                  href="https://data.cityofnewyork.us/Public-Safety/NYPD-Arrests-Data-Historic-/8h9b-rp9u"
+                  aria-label="NYCOpenData"
+                  title="NYCOpenData"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  NYC OpenData
+                </a>
+              </p>
             </div>
             <div className="col-md-8">
-            <div className="card">
-              <MapBox results={this.state.filtered} />
-            </div>
+              <div className="card">
+                <MapBox results={this.state.filtered} />
+              </div>
             </div>
 
-       
             <div className="col-md-12">
-              <ResultList results={this.state.filtered} /> 
+              <ResultList results={this.state.filtered} />
             </div>
-        
+
+            <div className="col-md-12 text-center mb-2">
+            <button className="btn btn-outline-dark mr-1" onClick={() => this.advanceOffset(true)}>Prev</button>
+              <button className="btn btn-outline-dark ml-1" onClick={() => this.advanceOffset(false)}>Next</button>
+            </div>
+
           </div>
         </div>
       </>
--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,36 +1,57 @@
 import React from "react";
 import "./style.css";
 
-function SearchForm(props) {
-  const selector = () => {
-    let obj = {};
+function SearchForm({ results, handleInputChange }) {
+  // const selector = () => {
+  //   let obj = {};
 
-    if (props.results.length) {
-      for (let i = 0; i < props.results.length; i++) {
-        var key = props.results[i].ofns_desc;
-        if (key) obj[key] = true;
-      }
-    }
+  //   if (props.results.length) {
+  //     for (let i = 0; i < props.results.length; i++) {
+  //       var key = props.results[i].ofns_desc;
+  //       if (key) obj[key] = true;
+  //     }
+  //   }
 
-    return (
-      <div className="input-group mb-3">
-        <select
-          className="custom-select"
-          id="inputGroupSelect01"
-          onChange={props.handleInputChange}
-        >
-          <option value=""> Choose Arrest Type </option>
-          {Object.keys(obj).sort().map((ele, i) => (
-            <option key={i + "-el"} value={ele}>
-              {ele.toLowerCase()}
-            </option>
-          ))}
-        </select>
-      </div>
-    );
-  };
+  //   return (
+  //     <div className="input-group mb-3">
+  //       <select
+  //         className="custom-select"
+  //         id="inputGroupSelect01"
+  //         onChange={props.handleInputChange}
+  //       >
+  //         <option value=""> Choose Arrest Type </option>
+  //         {Object.keys(obj).sort().map((ele, i) => (
+  //           <option key={i + "-el"} value={ele}>
+  //             {ele.toLowerCase()}
+  //           </option>
+  //         ))}
+  //       </select>
+  //     </div>
+  //   );
+  // };
 
-  return <div> {selector()} </div>;
+  // return <div> {selector()} </div>;
+
+  return (
+    <div className="input-group mb-3">
+      <select
+        className="custom-select"
+        id="inputGroupSelect01"
+        onChange={handleInputChange}
+      >
+        <option value=""> Choose Arrest Type </option>
+        {results.map((ele, i) => (
+          <option key={i + "-el"} value={ele}>
+            {ele}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
+
+SearchForm.defaulProps = {
+  results: [],
+};
 
 export default SearchForm;
